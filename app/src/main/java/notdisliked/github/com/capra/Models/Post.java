e0fd080d56daf02234d6d1e15ff6b0b2634c8882
Jonathan Alexander <notdisliked@gmail.com>
2015-07-11 00:07:32 -0700

    Adjust padding on the post

---
+++ app/src/main/java/notdisliked/github/com/capra/Models/Post.java
@@ -0,0 +1,18 @@
+package notdisliked.github.com.capra.Models;
+
+/**
+ * Created by notdi on 7/10/2015.
+ */
+public class Post {
+    String Title;
+    int Likes;
+    int Dislikes;
+    String Name;
+    String Date;
+    String Subverse;
+    String Id;
+    String Content;
+
+
+
+}
